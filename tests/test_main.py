82cc0927bf2f60a933a44c631b203c397639c0fb
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-08 15:28:56 +0200

    dodanie nowego projektu

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import unittest
+from main import add,subtract
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(-1,1),0)
+
+        def test_subtract(self):
+            self.assertEqual(subtract(5,3),2)
+            self.assertEqual(subtract(2,4),-2)
+
+
+            if
+            if|_name_=='_main_':
+                unittest.main()
